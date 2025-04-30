02f78e29cdbdc943b7e7c11cdd41c70ffb02c344
Henry Reaño <henry29.2004@hotmail.com>
2025-04-30 00:07:04 -0500

    fix: change copyright internationalization in the footer

--- src/public/components/footer.component.vue
+++ src/public/components/footer.component.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <footer class="footer-container">
     <div class="footer-section">
-      <p>Copyright &copy; 2024 Rick and Morty. All rights reserved.</p>
+      <p>Copyright &copy; 2024 Rick and Morty. {{ $t('footer.right') }}.</p>
     </div>
     <div class="footer-section">
       <p>
